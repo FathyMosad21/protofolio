e594e588d779e5a9b2b87954034ead6200d6e9b4
FathyMosad21 <161249529+FathyMosad21@users.noreply.github.com>
2025-02-20 20:43:03 +0200

    Update gym.js

--- Gym/gym.js
+++ Gym/gym.js
@@ -104,3 +104,69 @@ var swiper = new Swiper(".home-slider", {
     },
   },
   });
+var swiper = new Swiper(".trainer-solider", {
+    spaceBetween: 20,
+    grabcursor: true,
+   loop: true,
+   centeredslides: true,
+   autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+        slidesperview: 1,
+    },
+    768: {
+        slidesperview: 2,
+    },
+    991: {
+        slidesperview: 3,
+    },
+  },
+  }); 
+  //for blogs
+  var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabcursor: true,
+   loop: true,
+   centeredslides: true,
+   autoplay: {
+    delay: 8500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+        slidesperview: 1,
+    },
+    768: {
+        slidesperview: 2,
+    },
+    991: {
+        slidesperview: 3,
+    },
+  },
+  });
+  // حدد العناصر
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.nav');
+
+// إضافة حدث النقر
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+// إغلاق القائمة عند التمرير
+window.onscroll = () => {
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+// إغلاق القائمة عند النقر على أي رابط
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuBtn.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    });
+});
