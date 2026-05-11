2a07aa02fd2d5065bb52fa86a3def097a98e3cfe
FathyMosad21 <161249529+FathyMosad21@users.noreply.github.com>
2026-05-12 00:03:17 +0300

    Update proto.js

---
+++ proto.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const btn = document.getElementById('burger');
+    const drawer = document.getElementById('drawer');
+    btn.classList.toggle('open');
+    drawer.classList.toggle('open');
+}
+
+function closeMenu() {
+    document.getElementById('burger').classList.remove('open');
+    document.getElementById('drawer').classList.remove('open');
+}
